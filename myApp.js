a579457055f4061a5289b2a85729ce61d3a7f975
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-12 15:12:59 +0630

    Add .env file

--- myApp.js
+++ myApp.js
@@ -2,13 +2,17 @@ let express = require('express');
 let app = express();
 let absolutePath = __dirname + "/views/index.html";
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname+"/public"))
 // app.get("/", (req, res)=>{
 //     res.send("Hello Express")
 // })
 
 app.get("/", (req, res)=>{
     res.sendFile(absolutePath)
+})
+
+app.get('/json',(req, res)=>{
+  res.json({"message": process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello Json"})
 })
 console.log("Hello World");
 
