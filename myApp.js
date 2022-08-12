8e558a99865703fecac8bd097bed78b874de9ef5
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-12 13:08:22 +0630

    Add app.use()

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 let absolutePath = __dirname + "/views/index.html";
 
+app.use(express.static(__dirname+"/public"))
 // app.get("/", (req, res)=>{
 //     res.send("Hello Express")
 // })
